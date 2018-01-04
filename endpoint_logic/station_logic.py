9208a7a71fa8dad75fe44fcd5b7ee65b9cc42868
Jonathan Chiou <jchio001@ucr.edu>
2018-01-03 17:39:02 -0800

    Cleaned up station info logic

--- endpoint_logic/station_logic.py
+++ endpoint_logic/station_logic.py
@@ -5,86 +5,52 @@ from misc.constants import RESP_HEADER
 
 import json
 
+activities = ['food', 'shopping', 'attraction']
+
 
 def get_stations(bart_api_key):
     return json.dumps({'stations': bart_api_client.get_stations(bart_api_key=bart_api_key)}), constants.HTTP_STATUS_OK, \
            RESP_HEADER
 
 
-def format_station_info_dict(station_info=None):
-    if station_info is None:
-        return
+# Formats platform information for a specific direction from BART API into a much cleaner format
+def format_platform_info(station_info, direction):
+    platform_info = OrderedDict()
+    if '{}_platforms'.format(direction) in station_info and '{}_routes'.format(direction) in station_info:
+        platform_info['platforms'] = station_info['{}_platforms'.format(direction)]['platform'] \
+            if station_info['{}_platforms'.format(direction)] != '' else []
+        platform_info['routes'] = station_info['{}_routes'.format(direction)]['route']
 
-    ordered_dict = OrderedDict()
-    ordered_dict['link'] = station_info['link']
-    ordered_dict['abbr'] = station_info['abbr']
-    ordered_dict['name'] = station_info['name']
-    ordered_dict['description'] = station_info['description']
-    ordered_dict['lat'] = station_info['lat']
-    ordered_dict['lng'] = station_info['lng']
-    ordered_dict['address'] = station_info['address']
-    ordered_dict['city'] = station_info['city']
-    ordered_dict['state'] = station_info['state']
-    ordered_dict['zipcode'] = station_info['zipcode']
-    ordered_dict['north_info'] = station_info['north_info']
-    ordered_dict['south_info'] = station_info['south_info']
-    ordered_dict['activities'] = station_info['activities']
-
-    return ordered_dict
+    return platform_info
 
 
-# TODO: Station info probably doesn't change so I only need to perform this computation once and shove it into PSQL
-# TODO: Also find out how to hook flask up with PSQL on heroku
+def format_activity(station_info, activity):
+    activity_info = OrderedDict()
+    activity_info['activity_type'] = activity
+    activity_info['info'] = station_info[activity]['#cdata-section']
+    return activity_info
+
+
+def format_station_info_resp(station_info):
+    formatted_station_info_dict = OrderedDict()
+    formatted_station_info_dict['link'] = station_info['link']['#cdata-section']
+    formatted_station_info_dict['abbr'] = station_info['abbr']
+    formatted_station_info_dict['name'] = station_info['name']
+    formatted_station_info_dict['description'] = station_info['intro']['#cdata-section']
+    formatted_station_info_dict['lat'] = station_info['gtfs_latitude']
+    formatted_station_info_dict['lng'] = station_info['gtfs_longitude']
+    formatted_station_info_dict['address'] = station_info['address']
+    formatted_station_info_dict['city'] = station_info['city']
+    formatted_station_info_dict['state'] = station_info['state']
+    formatted_station_info_dict['zipcode'] = station_info['zipcode']
+    formatted_station_info_dict['north_info'] = format_platform_info(station_info=station_info, direction='north')
+    formatted_station_info_dict['south_info'] = format_platform_info(station_info=station_info, direction='south')
+    formatted_station_info_dict['activities'] = [map(lambda a : format_activity(station_info, a), activities)]
+
+    return formatted_station_info_dict
+
+
 def get_station_info(station_abbr, bart_api_key):
     station_info = bart_api_client.get_station_info(station_abbr, bart_api_key)
-
-    del station_info['platform_info']
-
-    station_info['lat'] = station_info['gtfs_latitude']
-    station_info['lng'] = station_info['gtfs_longitude']
-    del station_info['gtfs_latitude'], station_info['gtfs_longitude']
-
-    station_info['description'] = station_info['intro']['#cdata-section']
-    del station_info['intro']
-
-    station_info['link'] = station_info['link']['#cdata-section']
-
-    del station_info['cross_street']
-
-    # TODO: this is repetitive. Make a method that handles creating the north_info/south_info dict
-    if 'north_platforms' in station_info and 'north_routes' in station_info:
-        north_info = {}
-        north_info['platforms'] = station_info['north_platforms']['platform'] \
-            if station_info['north_platforms'] != '' else []
-        north_info['routes'] = station_info['north_routes']['route']
-        station_info['north_info'] = north_info
-        del station_info['north_platforms'], station_info['north_routes']
-
-    south_info = {}
-    south_info['platforms'] = station_info['south_platforms']['platform'] \
-        if station_info['south_platforms'] != '' else []
-    south_info['routes'] = station_info['south_routes']['route']
-    station_info['south_info'] = south_info
-    del station_info['south_platforms'], station_info['south_routes']
-
-    # TODO: modularize this
-    food_info = OrderedDict()
-    food_info['activity_type'] = 'food'
-    food_info['info'] = station_info['food']['#cdata-section']
-    del station_info['food']
-
-    shopping_info = OrderedDict()
-    shopping_info['activity_type'] = 'shopping'
-    shopping_info['info'] = station_info['shopping']['#cdata-section']
-    del station_info['shopping']
-
-    attraction_info = OrderedDict()
-    attraction_info['activity_type'] = 'attraction'
-    attraction_info['info'] = station_info['attraction']['#cdata-section']
-    del station_info['attraction']
-
-    activities = [food_info, shopping_info, attraction_info]
-    station_info['activities'] = activities
-
-    return json.dumps({'station_info': format_station_info_dict(station_info)}), constants.HTTP_STATUS_OK, \
+    return json.dumps({'station_info': format_station_info_resp(station_info)}), constants.HTTP_STATUS_OK, \
            RESP_HEADER
